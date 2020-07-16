efe78ef536c8aa7bd44473cf811255ab13d66f4f
kolja.schuemann <kolja.schuemann@scopevisio.com>
2020-07-17 00:11:33 +0200

    initial add

---
+++ sv/src/main/java/com/scopeonspring/sv/SvApplication.java
@@ -0,0 +1,13 @@
+package com.scopeonspring.sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvApplication.class, args);
+	}
+
+}
---
+++ sv/src/test/java/com/scopeonspring/sv/SvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scopeonspring.sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
